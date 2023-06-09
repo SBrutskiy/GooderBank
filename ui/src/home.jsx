36f527637e94672dec5ad3829c65b36a00cd61ad
SBrutskiy <96359894+SBrutskiy@users.noreply.github.com>
2023-06-08 20:00:18 -0500

    fix typo

--- ui/src/home.jsx
+++ ui/src/home.jsx
@@ -5,7 +5,7 @@ export function Home() {
   return (
     <Card
       txtcolor="black"
-      header="The Gooder Banks"
+      header="The Gooder Bank"
       title="Welcome to the bank"
       text="You can move around using the navigation bar."
       body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
